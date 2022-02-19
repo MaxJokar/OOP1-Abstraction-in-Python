e619013929a0ebb1668a0b4d1300bc9816ff6f5f
email <mmb2020max@gmail.com>
2022-02-19 19:22:12 +0200

    Getter Setter2

--- oop2.py
+++ oop2.py
@@ -7,3 +7,22 @@ class Person :
         print(self.__name)  
         print(self.__age)  
         
+    def getName(self):
+        return self.__name    
+    
+    def setName(self,name):
+        self.__name=name 
+    
+    
+    
+person1=Person("Rose",72)
+person1.showPersonInfo()
+
+print(person1.getName())
+person1.setName("Philip")
+person1.showPersonInfo()  
+    
+    
+    
+    
+    
