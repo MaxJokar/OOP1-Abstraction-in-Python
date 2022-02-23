9082256e217b84651376d01d67abb0715c9b8dc1
email <mmb2020max@gmail.com>
2022-02-23 12:02:36 +0200

    LAST TWEAK DONE

--- oop3.py
+++ oop3.py
@@ -6,7 +6,7 @@ class Person(ABC):
     def __init__(self, name, age,Nationality):
         self._name = name
         self._age = age
-        self.__nationality=Nationality
+        self.__nationality=Nationality #Can not be Accessed By Children if the field is  PRIVATE!
         
     @abstractmethod  # This function (Method) must be used in Children,could be with differet codes
     def showInfo(self):
