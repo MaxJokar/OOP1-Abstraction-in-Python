36198b61dd788b94aaa54c31a46e92dd647e4703
email <mmb2020max@gmail.com>
2022-02-23 11:29:12 +0200

    Last Tweak Done

--- oop.py
+++ oop.py
@@ -1,15 +1,15 @@
 class Person:
-    #example(Instance) of  an instance method:(data of the class are name or age )
+    #example of  an instance method:(data of the class are name or age )
     def __init__(self,name,age): 
         self.name=name
         self.age=age
         
-    def showPersonInfo(self):  #showPersonInfo:instance method depends on the method
+    def showPersonInfo(self):  
         print(self.name)    
         print(self.age)
         
     #independant Method(Static method) :dosent Depend to the Class 
-    #It doesnt belog to person1
+    #It doesnt belong to person1
     @staticmethod 
     def sum(x,y):
         return x+y 
@@ -18,12 +18,11 @@ class Person:
     #cls shows it belogs to what class(we access to the static or static values of the class)
     @classmethod 
     def fun1(cls,x,y):
-        
         return cls.sum(x,y)+2000        
         
   
-# person1=Person("Jack",48)
-# person1.showPersonInfo()        
+person1=Person("Jack",48)
+person1.showPersonInfo()        
 #Static method are called by the Class and not instance of Class
 # print(Person.sum(20,50))
 print(Person.fun1(3,25))
